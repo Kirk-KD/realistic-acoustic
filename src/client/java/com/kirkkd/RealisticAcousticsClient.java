7204501e7e51247c5f3a628214e60a5388af814f
Kirk-KD <kirk.ding123@gmail.com>
2025-04-15 18:23:11 -0400

    Initial

--- src/client/java/com/kirkkd/RealisticAcousticsClient.java
+++ src/client/java/com/kirkkd/RealisticAcousticsClient.java
@@ -3,11 +3,6 @@ package com.kirkkd;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.SoundManager;
-import org.lwjgl.system.MemoryStack;
-
-import java.io.Console;
-
-import static org.lwjgl.openal.ALC10.*;
 
 public class RealisticAcousticsClient implements ClientModInitializer {
 	public static MinecraftClient MC_CLIENT;
@@ -20,17 +15,6 @@ public class RealisticAcousticsClient implements ClientModInitializer {
 
 	public static void onJoinWorld() {
 		SOUND_MANAGER = MC_CLIENT.getSoundManager();
-
-		try (MemoryStack stack = MemoryStack.stackPush()) {
-			long device = alcGetContextsDevice(alcGetCurrentContext());
-			boolean efxSupported = alcIsExtensionPresent(device, "ALC_EXT_EFX");
-			if (!efxSupported) {
-				throw new IllegalStateException("EFX extension not supported by this OpenAL implementation.");
-			}
-
-			RealisticAcoustics.LOGGER.info("EFX is supported!");
-		}
-
 	}
 
 	public static void onDisconnect() {
